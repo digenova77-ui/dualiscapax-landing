4abab90a026193f24d98cba490460d99e3eb8d22
digenova77-ui <digenova77@gmail.com>
2026-08-29 02:21:43 -0400

    Same 0x on Polygon research rail

--- js/payments-config.js
+++ js/payments-config.js
@@ -1,4 +1,4 @@
-/** DualisCapax payment rails. Addresses are copy-on-tap only. */
+/** DualisCapax payment rails. Addresses copy-on-tap only. */
 window.DC_PAYMENTS = {
   interac_enabled: true,
   eft_enabled: true,
@@ -12,5 +12,6 @@ window.DC_PAYMENTS = {
   research_xrp: "rNbfUBt3vgUDu9kEL5TxbgLHxtKbyyKyRz",
   research_zec: "t1XXgbcak1o85jd4PWwBzatqxbDSkuwSTaZ",
   research_sol: "Cus1pLggfxDJC8FJSQv1tRtzDanuzSxjWvSsKA12byDw",
-  research_eth: "0x0adC5f2Dcb239DAAF3eeB3cc34b3F1BFF5AFBBc4"
+  research_eth: "0x0adC5f2Dcb239DAAF3eeB3cc34b3F1BFF5AFBBc4",
+  research_pol: "0x0adC5f2Dcb239DAAF3eeB3cc34b3F1BFF5AFBBc4"
 };
