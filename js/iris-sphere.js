a29b2f4389fd75f6ee9aab618de7165f8e8c98b9
David J. Di Genova <digenova77@gmail.com>
2026-09-22 09:16:23 -0400

    feat(iris): quaternion sphere + DSAP wake on existing lander (factory-ready)

---
+++ js/iris-sphere.js
@@ -0,0 +1,5 @@
+/** Mirror of cf-pages/js/iris-sphere.js */
+(function (w) {
+  var s = document.createElement("script");
+  s.src = "/js/iris-sphere.js".indexOf("cf-pages") >= 0 ? "js/iris-sphere.js" : "/js/iris-sphere.js";
+})(window);
