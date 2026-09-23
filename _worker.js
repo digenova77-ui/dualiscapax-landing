cb626e395b042797ae7a966e46cf4384252317df
David J. Di Genova <digenova77@gmail.com>
2026-09-23 13:53:02 -0400

    L0: Pages Advanced Mode _worker.js fail-open ASSETS. Map ice/look/rink/rte/sima. Home unmapped.

--- _worker.js
+++ _worker.js
@@ -1,7 +1,4 @@
-/* dualiscapax-web L0
- * Pretty stems as 200 files. Home and unknown paths fall through.
- * If this file throws, catch and serve assets so the zone does not die.
- */
+/* Pages Advanced Mode. Fail-open to ASSETS. Home never mapped. */
 const MAP = {
   "/ice": "/rink.html",
   "/ice/": "/rink.html",
@@ -12,41 +9,32 @@ const MAP = {
   "/rink": "/rink.html",
   "/rink/": "/rink.html",
   "/rink.html": "/rink.html",
-  "/hockey": "/hockey.html",
-  "/hockey/": "/hockey.html",
+  "/hockey": "/rink.html",
+  "/hockey/": "/rink.html",
   "/alacarte": "/alacarte.html",
   "/alacarte/": "/alacarte.html",
-  "/menu": "/alacarte.html",
+  "/alacarte.html": "/alacarte.html",
   "/sima": "/rte/sima-dclm/index.html",
   "/sima/": "/rte/sima-dclm/index.html",
   "/rte": "/rte/index.html",
   "/rte/": "/rte/index.html",
   "/aide": "/rte/sima-dclm/index.html",
+  "/aide/": "/rte/sima-dclm/index.html",
   "/desk": "/rte/sima-dclm/index.html",
-  "/why": "/why.html",
-  "/why/": "/why.html",
-  "/sheet": "/sheet.html",
-  "/sheet/": "/sheet.html",
-  "/gates": "/gates.html",
-  "/gates/": "/gates.html"
+  "/desk/": "/rte/sima-dclm/index.html"
 };
 
 export default {
   async fetch(request, env) {
+    const url = new URL(request.url);
+    const dest = MAP[url.pathname];
     try {
-      const url = new URL(request.url);
-      const dest = MAP[url.pathname];
-      if (dest && env && env.ASSETS) {
-        const assetUrl = new URL(dest, url.origin);
-        const res = await env.ASSETS.fetch(new Request(assetUrl.toString(), request));
-        const headers = new Headers(res.headers);
-        headers.delete("Location");
-        if (res.ok) {
-          return new Response(res.body, { status: 200, headers });
-        }
+      if (dest && env.ASSETS) {
+        url.pathname = dest;
+        return env.ASSETS.fetch(new Request(url.toString(), request));
       }
-    } catch (err) {
-      /* fail open */
+    } catch (e) {
+      /* fail-open */
     }
     return env.ASSETS.fetch(request);
   }
